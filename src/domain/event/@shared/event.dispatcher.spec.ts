705db7385c6bd783b80443beb8d2ba512072bfc5
luizmoitinho <luizcarlos_costam@hotmail.com>
2025-01-22 23:14:37 -0300

    feat: adding domain events

---
+++ src/domain/event/@shared/event.dispatcher.spec.ts
@@ -0,0 +1,14 @@
+import { SendEmailWhenProductIsCreatedHandler } from "../product/handler/send.email.product.create";
+import { EventDispatcher } from "./event.dispatcher";
+
+describe('Domain events tests', ()=>{
+    it('should register an event handler', () => {
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+
+        eventDispatcher.register('product-created-event', eventHandler);
+
+        expect(eventDispatcher.getEventHandlers['product-created-event']).toBeDefined();
+        expect(eventDispatcher.getEventHandlers['product-created-event'].length).toBe(1);
+    })
+})
