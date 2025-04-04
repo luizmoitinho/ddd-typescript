6b8bb1724dad181b1cd7bd8707f1e9bbc0708bec
luizmoitinho <luizcarlos_costam@hotmail.com>
2025-04-03 23:04:29 -0300

    feat: add create customer use case

---
+++ src/usecase/customer/create/create.customer.usecase.ts
@@ -0,0 +1,33 @@
+import { Customer } from "../../../domain/customer/entity/customer";
+import { CustomerFactory } from "../../../domain/customer/factory/customer.factory";
+import CustomerRepositoryInterface from "../../../domain/customer/repository/customer.repository.interface";
+import { Address } from "../../../domain/customer/value-object/address";
+import { InputCreateCustomerDto, OutputCreateCustomerDto } from "./create.customer.dto";
+ import { v4 as uuid } from "uuid";
+
+export class CreateCustomerUseCase {
+    private customerRepository: CustomerRepositoryInterface
+
+    constructor(repository: CustomerRepositoryInterface){
+        this.customerRepository = repository
+    }
+
+    async execute(input: InputCreateCustomerDto): Promise<OutputCreateCustomerDto>{
+        const customer = CustomerFactory.createWithAddress(
+            input.name,
+            new Address(input.address.street, input.address.number, input.address.zipCode, input.address.city)
+        );
+        await this.customerRepository.create(customer)
+
+        return <OutputCreateCustomerDto>{
+            id: customer.id,
+            name: customer.name,
+            address: {
+                street: customer.Address.street,
+                number: customer.Address.number,
+                city: customer.Address.city,
+                zipCode: customer.Address.zipCode
+            }
+        }
+    }
+}
